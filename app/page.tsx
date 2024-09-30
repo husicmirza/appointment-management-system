bbb352273092c63cfe83e2e4bc4bdeb5e4a1b007
husicmirza <mirza.huc@gmail.com>
2024-09-30 14:35:59 +0300

    Add home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,37 @@
-import { Button } from "@/components/ui/button";
+import PatientForm from "@/components/forms/PatientForm";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-3xl">Home</h1>
-      <Button>CN</Button>
+    <div className="flex h-screen max-h-screen">
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
+          <Image
+            src={"./logo.svg"}
+            alt={"logo"}
+            width={1000}
+            height={1000}
+            className="mb-12 h-10 w-fit"
+          />
+          <PatientForm></PatientForm>
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl:text-left">
+              © Mirza Husic
+            </p>
+            <Link href="/?admin=true" className="text-green-500">
+              Admin
+            </Link>
+          </div>
+        </div>
+      </section>
+      <Image
+        src="/assets/images/onboarding-img.png"
+        alt="patient"
+        height={1000}
+        width={1000}
+        className="side-img max-w-[50%] rounded-3xl"
+      />
     </div>
   );
 }
