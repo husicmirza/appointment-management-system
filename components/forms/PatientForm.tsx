c8c8df9aee75445e617ed970609825d5679ded11
husicmirza <mirza.huc@gmail.com>
2024-10-04 17:48:26 +0300

    Create reusable form fields and add user login form

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -1,7 +1,73 @@
-import React from "react";
+"use client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+
+import { z } from "zod";
+import { Form } from "@/components/ui/form";
+import CustomFormField, { FormFieldType } from "../CustomFormField";
+import SubmitButton from "../SubmitButton";
+import { UserFormValidation } from "@/lib/validation";
 
 const PatientForm = () => {
-  return <div>PatientForm</div>;
+  const [isLoading, setIsLoading] = useState(false);
+  const form = useForm<z.infer<typeof UserFormValidation>>({
+    resolver: zodResolver(UserFormValidation),
+    defaultValues: {
+      name: "",
+      email: "",
+      phone: "",
+    },
+  });
+
+  function onSubmit({
+    name,
+    email,
+    phone,
+  }: z.infer<typeof UserFormValidation>) {
+    setIsLoading(true);
+
+    try {
+      const userData = { name, email, phone };
+      console.log(userData);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
+        <section className="mb-12 space-y-4">
+          <h1 className="header">Hi there</h1>
+          <p className="text-dark-700">Schedule your first appointment.</p>
+        </section>
+        <CustomFormField
+          control={form.control}
+          fieldType={FormFieldType.INPUT}
+          name="name"
+          placeholder="Mirza Husic"
+          label="Full Name"
+          iconSrc="./assets/icons/user.svg"
+        />
+
+        <CustomFormField
+          control={form.control}
+          fieldType={FormFieldType.INPUT}
+          name="email"
+          placeholder="example@gmail.com"
+          label="Email"
+          iconSrc="./assets/icons/email.svg"
+        />
+        <CustomFormField
+          control={form.control}
+          fieldType={FormFieldType.PHONE_INPUT}
+          name="phone"
+          label="Phone"
+        />
+        <SubmitButton isLoading={isLoading}>Get Started</SubmitButton>
+      </form>
+    </Form>
+  );
 };
 
 export default PatientForm;
