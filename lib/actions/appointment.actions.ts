fa7e18aae3b074822f1cc089591862cab41855f6
husicmirza <mirza.huc@gmail.com>
2024-10-08 14:13:04 +0300

    Add create appointemnt success page

--- lib/actions/appointment.actions.ts
+++ lib/actions/appointment.actions.ts
@@ -27,3 +27,16 @@ export const createAppointment = async (
     console.error("An error occurred while creating a new appointment:", error);
   }
 };
+
+export const getAppointment = async (appointmentId: string) => {
+  try {
+    const appointment = await database.getDocument(
+      DATABASE_ID!,
+      APPOINTMENT_COLLECTION_ID!,
+      appointmentId
+    );
+    return parseStringify(appointment);
+  } catch (error) {
+    console.log(error);
+  }
+};
