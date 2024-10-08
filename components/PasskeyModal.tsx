50d8858f1da7cf89d56b48c5fb19acfe08d713a6
husicmirza <mirza.huc@gmail.com>
2024-10-08 14:54:32 +0300

    Implementation for admin verification otp

---
+++ components/PasskeyModal.tsx
@@ -0,0 +1,114 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@/components/ui/alert-dialog";
+import Image from "next/image";
+import { usePathname, useRouter } from "next/navigation";
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSlot,
+} from "@/components/ui/input-otp";
+import { decryptKey, encryptKey } from "@/lib/utils";
+
+const PasskeyModal = () => {
+  const [open, setOpen] = useState(true);
+  const [passkey, setPassKey] = useState("");
+  const [error, setError] = useState("");
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const encryptedKey =
+    typeof window !== "undefined"
+      ? window.localStorage.getItem("accessKey")
+      : null;
+
+  useEffect(() => {
+    const accessKey = encryptedKey && decryptKey(encryptedKey);
+
+    if (pathname)
+      if (accessKey === process.env.NEXT_PUBLIC_ADMIN_PASSKEY) {
+        setOpen(false);
+        router.push("/admin");
+      } else {
+        setOpen(true);
+      }
+  }, [encryptedKey]);
+
+  const validatePassKey = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    e.preventDefault();
+    if (passkey === process.env.NEXT_PUBLIC_ADMIN_PASSKEY) {
+      const encryptedKey = encryptKey(passkey);
+      localStorage.setItem("accessKey", encryptedKey);
+      setOpen(false);
+    } else {
+      setError("Invalid passkey. Try again!");
+    }
+  };
+
+  const closeModal = () => {
+    setOpen(false);
+    router.push("/");
+  };
+
+  return (
+    <AlertDialog open={open} onOpenChange={setOpen}>
+      <AlertDialogContent className="shad-alert-dialog">
+        <AlertDialogHeader>
+          <AlertDialogTitle className="flex items-start justify-between">
+            Admin access verification
+            <Image
+              src={"/assets/icons/close.svg"}
+              alt="close"
+              width={20}
+              height={20}
+              onClick={closeModal}
+              className="cursor-pointer"
+            />
+          </AlertDialogTitle>
+          <AlertDialogDescription>
+            To access admin page, please enter the passkey.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <div className="flex justify-center flex-col">
+          <InputOTP
+            maxLength={6}
+            value={passkey}
+            onChange={(value) => setPassKey(value)}
+          >
+            <InputOTPGroup className="shad-otp">
+              <InputOTPSlot className="shad-otp-slot" index={0} />
+              <InputOTPSlot className="shad-otp-slot" index={1} />
+              <InputOTPSlot className="shad-otp-slot" index={2} />
+              <InputOTPSlot className="shad-otp-slot" index={3} />
+              <InputOTPSlot className="shad-otp-slot" index={4} />
+              <InputOTPSlot className="shad-otp-slot" index={5} />
+            </InputOTPGroup>
+          </InputOTP>
+          {error && (
+            <p className="shad-error text-14-regular mt-4 flex">{error}</p>
+          )}
+        </div>
+        <AlertDialogFooter>
+          <AlertDialogAction
+            className="shad-primary-btn w-full"
+            onClick={(e) => validatePassKey(e)}
+          >
+            Enter Admin Passkey
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
+
+export default PasskeyModal;
